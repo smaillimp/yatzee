aab6d93a938b5826687364ac2f5e75c0c5863184
Andy Moesch <andy.moesch@hotmail.com>
2020-05-23 23:04:52 +0200

    add player name query

--- user.py
+++ user.py
@@ -1,9 +1,15 @@
-def game_input(question, input_type):
+def game_input(question, input_type=None):
     value = input("{} ".format(question))
-    return_value = input_type(value)
-    return return_value
+    return input_type(value) if input_type is not None else value
 
 
 if __name__ == "__main__":
-    number_of_players = game_input("How many players will play the game?", int)
+    number_of_players = game_input(
+        "How many players will play the game?", input_type=int
+    )
     print(number_of_players)
+    players = list()
+    for i in range(number_of_players):
+        player_name = game_input("What is player {}'s name?".format(i + 1))
+        players.append(player_name)
+    print(players)
