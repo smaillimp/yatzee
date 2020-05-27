913a24b5c860a3cc38e2b74981768313db4848cf
Andy Moesch <andy.moesch@hotmail.com>
2020-05-27 19:17:04 +0200

    fix test three of a kind

--- combinations.py
+++ combinations.py
@@ -1,5 +1,9 @@
 def get_number_of_eyes_for_number(roll, number):
-    return [roll[key] for key in roll].count(number) * number
+    return get_number_of_hits_for_number(roll, number) * number
+
+
+def get_number_of_hits_for_number(roll, number):
+    return [roll[key] for key in roll].count(number)
 
 
 def get_aces(roll):
@@ -24,3 +28,13 @@ def get_fives(roll):
 
 def get_sixes(roll):
     return get_number_of_eyes_for_number(roll, 6)
+
+
+def get_three_of_a_kind(roll):
+    number_of_hits = dict()
+    for i in range(1, 7):
+        number_of_hits[i] = get_number_of_hits_for_number(roll, i)
+        if number_of_hits[i] == 3:
+            return number_of_hits[i] * i
+    else:
+        return 0
--- test/test_combinations.py
+++ test/test_combinations.py
@@ -44,22 +44,36 @@ def test_get_sixes():
     assert combinations.get_sixes(roll) == 12
 
 
-def test_get_three_of_a_kind():
+def test_get_three_of_a_kind_with_3_2s():
 
     roll = {0: 2, 1: 2, 2: 2, 3: 3, 4: 4}
 
-    assert combinations.get_three_of_a_kind() == 6
+    assert combinations.get_three_of_a_kind(roll) == 6
 
 
-def test_get_three_of_a_kind():
+def test_get_three_of_a_kind_with_3_4s():
 
     roll = {0: 3, 1: 4, 2: 4, 3: 5, 4: 4}
 
-    assert combinations.get_three_of_a_kind() == 12
+    assert combinations.get_three_of_a_kind(roll) == 12
 
 
-def test_get_three_of_a_kind():
+def test_get_three_of_a_kind_with_no_triple():
 
     roll = {0: 1, 1: 2, 2: 3, 3: 3, 4: 4}
 
-    assert combinations.get_three_of_a_kind() == 0
+    assert combinations.get_three_of_a_kind(roll) == 0
+
+
+def test_get_four_of_a_kind_with_4_6s():
+
+    roll = {0: 6, 1: 6, 2: 6, 3: 3, 4: 6}
+
+    assert combinations.get_four_of_a_kind(roll) == 24
+
+
+def test_get_four_of_a_kind_with_no_quadruple():
+
+    roll = {0: 6, 1: 6, 2: 2, 3: 3, 4: 6}
+
+    assert combinations.get_four_of_a_kind(roll) == 0
